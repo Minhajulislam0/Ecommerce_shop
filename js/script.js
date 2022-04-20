9e8fb616a1622ff3b792392249914a655a0e4f6b
Minhajulislam0 <mdmishu98730@gmail.com>
2022-04-20 18:04:50 +0600

    Responsive shop with a pagination

--- js/script.js
+++ js/script.js
@@ -13,3 +13,24 @@ if(closebtn){
         nav.classList.remove('active');
     })
 }
+
+// single product
+
+const mainImg = document.getElementById('main-img');
+const subImg = document.getElementsByClassName('small-img');
+
+subImg[0].onclick = function(){
+    mainImg.src = subImg[0].src;
+}
+subImg[1].onclick = function(){
+    mainImg.src = subImg[1].src;
+}
+subImg[2].onclick = function(){
+    mainImg.src = subImg[2].src;
+}
+subImg[3].onclick = function(){
+    mainImg.src = subImg[3].src;
+}
+subImg[4].onclick = function(){
+    mainImg.src = subImg[4].src;
+}
